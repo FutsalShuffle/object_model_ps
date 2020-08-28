67289755019474ac2161a00d7df2b08f7a4c1e6e
Andrey Burdey <nrgnevskyfrontak@gmail.com>
2020-08-28 17:19:02 +0500

    3rd fix

--- object.php
+++ object.php
@@ -1,3 +1,5 @@
+<?php
+
 class StudentsCore extends ObjectModel
 {
     public $id_students;
@@ -11,34 +13,30 @@ class StudentsCore extends ObjectModel
     'primary' => 'id_students',
     'multilang' => true,
     'fields' => array(
-        'name'         => array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isGenericName'),
-        'bday_date'    => array('type' => self::TYPE_DATE),
-        'is_studying'  => array('type' => self::TYPE_BOOL),
-        'avg_score'    => array('type' => self::TYPE_FLOAT),
+        'name' => array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isGenericName'),
+        'bday_date' => array('type' => self::TYPE_DATE),
+        'is_studying' => array('type' => self::TYPE_BOOL),
+        'avg_score' => array('type' => self::TYPE_FLOAT),
     ),
     );
 
     public function __construct($idLang = null, $idShop = null)
     {
         parent::__construct($idLang, $idShop);
-        //?? вероятно не нужно
         if (Shop::getContext() != Shop::CONTEXT_SHOP) {
             $idShop = Shop::getContextListShopID();
-            } else {
-                $id = Context::getContext()->shop->id;
-                $idShop = [$id ? $id : Configuration::get('PS_SHOP_DEFAULT')];
-            }
-        //
+        } else {
+            $id = Context::getContext()->shop->id;
+            $idShop = [$id ? $id : Configuration::get('PS_SHOP_DEFAULT')];
+        } 
     };
 
     public function getAll()
     {
         $return = null;
         $return &= Db::getInstance()->execute(
-            sprintf(
-            SELECT `*`
-            FROM `' . _DB_PREFIX_ . 'students`
-            )
+            "SELECT `*`
+            FROM `' . _DB_PREFIX_ . 'students`"
             );
         return $return;    
     };
@@ -46,29 +44,18 @@ class StudentsCore extends ObjectModel
     public function geBestScore()
     {
         $return = null;
-        $return &= Db::getInstance()->execute(
-            sprintf(
-            SELECT `MAX(avg_score)`
-            FROM `' . _DB_PREFIX_ . 'students`
-            )
-            );
+        $return &= Db::getInstance()->execute("SELECT `MAX(avg_score)` FROM `' . _DB_PREFIX_ . 'students`");
         return $return;      
     };
 
     public function getBestStudent()
     {
         $return = null;
-        $return &= Db::getInstance()->execute(
-            sprintf(
-            SELECT `*`
-            FROM `' . _DB_PREFIX_ . 'students` st
-            INNER JOIN ( 
-                SELECT `id_student`, MAX(avg_score)
-                FROM `' . _DB_PREFIX_ . 'students`
-                GROUP BY `id`
-            ) st2 ON `st.id_students` = `st2.id_students`
-            )
-            );
+        $return &= Db::getInstance()->execute("SELECT `*` FROM `' . _DB_PREFIX_ . 'students` st 
+        INNER JOIN (
+            SELECT `id_student`, MAX(avg_score) FROM `' . _DB_PREFIX_ . 'students`
+            GROUP BY `id_students`
+            ) st2 ON `st.id_students` = `st2.id_students`");
         return $return;      
     };
 }
