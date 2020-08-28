23d629f62da7fb26cab6b84941546618ce618ab8
Andrey Burdey <nrgnevskyfrontak@gmail.com>
2020-08-28 16:31:57 +0500

    fixes

--- object.php
+++ object.php
@@ -1,72 +1,75 @@
-class Students extends ObjectModel
+class StudentsCore extends ObjectModel
 {
-public static $definition = array(
-  'table' => 'students',
-  'primary' => 'id_students',
-  'multilang' => true,
-  'fields' => array(
-    'id_student'   => array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt'),
-    'name'         => array('type' => self::TYPE_STRING, 'validate' => 'isGenericName'),
-    'bday_date'    => array('type' => self::TYPE_DATE),
-    'active'       => array('type' => self::TYPE_BOOL),
-    'avg_score'    => array('type' => self::TYPE_FLOAT),
-    'meta_name' => array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isGenericName'),
-  ),
-);
+    public $id_students;
+    public $name;
+    public $bday_date;
+    public $is_studying = true;
+    public $avg_score;
+    public $meta_name;
 
-public function __construct($idLang = null, $idShop = null)
-{
-    parent::__construct($idLang, $idShop);
-    if (Shop::getContext() != Shop::CONTEXT_SHOP) {
-        $idShop = Shop::getContextListShopID();
-        } else {
-            $id = Context::getContext()->shop->id;
-            $idShop = [$id ? $id : Configuration::get('PS_SHOP_DEFAULT')];
-        }
-}
+    public static $definition = array(
+    'table' => 'students',
+    'primary' => 'id_students',
+    'multilang' => true,
+    'fields' => array(
+        'name'         => array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isGenericName'),
+        'bday_date'    => array('type' => self::TYPE_DATE),
+        'is_studying'  => array('type' => self::TYPE_BOOL),
+        'avg_score'    => array('type' => self::TYPE_FLOAT),
+    ),
+    );
 
-public function getByName(string $name)
-{
-    $return = null;
-    $return &= Db::getInstance()->execute(
-        sprintf(
-        SELECT `*`
-		FROM `' . _DB_PREFIX_ . 'students`
-		WHERE `name` = ' . $name
-        )
-        );
-    return $return;
-        
-}
+    public function __construct($idLang = null, $idShop = null)
+    {
+        parent::__construct($idLang, $idShop);
+        //?? вероятно не нужно
+        if (Shop::getContext() != Shop::CONTEXT_SHOP) {
+            $idShop = Shop::getContextListShopID();
+            } else {
+                $id = Context::getContext()->shop->id;
+                $idShop = [$id ? $id : Configuration::get('PS_SHOP_DEFAULT')];
+            }
+        //
+    };
 
-public function getMaxAvgScore()
-{
-    $return = null;
-    $return &= Db::getInstance()->execute(
-        sprintf(
-        SELECT `MAX(avg_score)`
-		FROM `' . _DB_PREFIX_ . 'students`
-        )
-        );
-    return $return;      
-}
-
-
-public function getBestStudent()
-{
-    $return = null;
-    $return &= Db::getInstance()->execute(
-        sprintf(
-        SELECT `*`
-		FROM `' . _DB_PREFIX_ . 'students` st
-        INNER JOIN ( 
-            SELECT `id_student`, MAX(avg_score)
+    public function getAll()
+    {
+        $return = null;
+        $return &= Db::getInstance()->execute(
+            sprintf(
+            SELECT `*`
             FROM `' . _DB_PREFIX_ . 'students`
-            GROUP BY `id`
-        ) st2 ON `st.id_student` = `st2.id_student`
-        )
-        );
-    return $return;      
-}
+            )
+            );
+        return $return;    
+    };
 
+    public function geBestScore()
+    {
+        $return = null;
+        $return &= Db::getInstance()->execute(
+            sprintf(
+            SELECT `MAX(avg_score)`
+            FROM `' . _DB_PREFIX_ . 'students`
+            )
+            );
+        return $return;      
+    };
+
+    public function getBestStudent()
+    {
+        $return = null;
+        $return &= Db::getInstance()->execute(
+            sprintf(
+            SELECT `*`
+            FROM `' . _DB_PREFIX_ . 'students` st
+            INNER JOIN ( 
+                SELECT `id_student`, MAX(avg_score)
+                FROM `' . _DB_PREFIX_ . 'students`
+                GROUP BY `id`
+            ) st2 ON `st.id_students` = `st2.id_students`
+            )
+            );
+        return $return;      
+    };
 }
